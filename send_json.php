2e32e863a5acff0e2d344cee4d4c6725d3abacc4
akshay <akshay.shet45@gmail.com>
2016-04-13 00:20:59 +0530

    Edited php

--- send_json.php
+++ send_json.php
@@ -1,15 +1,19 @@
 <?php
 
 
-$st=$_POST['route'];
-$wd=$_POST['date'];
-$wn=$_POST['time'];
-//$hr=$_POST['hr'];
-//$mn=$_POST['mn'];
+$event_id=$_POST['route'];
+$date=explode('-',$_POST['date']);
+$time=explode(':',$_POST['time']);
 
-
+$year=$date[0];
+$month=$date[1];
+$day=$date[2];
+$hr=$time[0];
+$mn=$time[1];
+$week_day= date("N", strtotime($_POST['date']) )-1;
+$week_number =floor(($day - 1) / 7 + 1);
 //$data = array($st,$wn, $wd);
-$data = array(13, 2, 9,0,1); //id,weekday,hr ,min,weekno
+$data = array($event_id, $week_day, $week_number,$hr,$mn); //id,weekday,hr ,min,weekno
 $result= shell_exec('python traffic_prediction.py ' . escapeshellarg(json_encode($data)));
 //$resultData = json_decode($result, true);
 //var_dump($data);
